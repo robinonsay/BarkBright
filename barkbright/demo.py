b047c9b98087ed82efcba39fea842c5d0e398db8
Robin Onsay <hello@robinonsay.com>
2023-05-06 17:46:19 -0500

    :sparkles: Add voice integration with dialogue

--- barkbright/demo.py
+++ barkbright/demo.py
@@ -20,13 +20,14 @@ import numpy as np
 from pathlib import Path
 from datetime import datetime
 from barkbright.models.intent import IntentMatchingModel
-from barkbright import Audio, Speaker, Microphone, CHUNK_SIZE, bb_config
+from barkbright import Audio, Speaker, Microphone, CHUNK_SIZE, bb_config, IN_RATE
 from barkbright import parsing, dialogue
 from dataset import BB_INTENTS
 from barkbright.models import asr
 from barkbright.iot.neopixel import NeoPixelLEDStrip
 from barkbright.colors import COLOR_MAP
 from multiprocessing import Pipe, Process
+from multiprocessing.connection import Connection
 
 def main(train=False):
     dialogue.load_dialogue()
@@ -45,22 +46,36 @@ def main(train=False):
     num_new_phrases = 0
     data = list()
     intent = None
-    parent_conn, child_conn = Pipe()
-    prcs = Process(target=process_speak, args=(child_conn,))
+    parent_mic_conn, child_mic_conn = Pipe()
+    prcs = Process(target=process_audio, args=(child_mic_conn,))
+    prcs.start()
+    parent_speaker_conn, child_speaker_conn = Pipe()
+    prcs = Process(target=process_speak, args=(child_speaker_conn,))
     prcs.start()
     with NeoPixelLEDStrip(**bb_config['led_config']) as np_leds:
         try:
-            for phrase in asr.listen():
+            for phrase, is_done in asr.listen(parent_mic_conn):
                 if not (phrase == '' or phrase is None):
                     if phrase == 'wakeword':
-                        parent_conn.send(phrase)
+                        parent_mic_conn.send({'run': True, 'record':False})
+                        parent_speaker_conn.send({'intent': phrase, 'reset': is_done})
+                        parent_speaker_conn.recv()
+                        parent_mic_conn.send({'run': True, 'record':True})
+                        np_leds[:] = COLOR_MAP['aqua']
+                    elif phrase == 'sleep_word':
+                        parent_speaker_conn.send({'intent': phrase, 'reset': is_done})
+                        parent_speaker_conn.recv()
+                        np_leds[:] = COLOR_MAP['black']
                     else:
                         sub_phrases = parsing.split_on_conj(phrase)
                         intent = intent_model.predict(sub_phrases)
                         for i, p in enumerate(sub_phrases):
                             intent_str = intent[i,0]
                             print(intent_str)
-                            parent_conn.send(intent_str)
+                            parent_mic_conn.send({'run': True, 'record':False})
+                            parent_speaker_conn.send({'intent': intent_str, 'reset': is_done})
+                            parent_speaker_conn.recv()
+                            parent_mic_conn.send({'run': True, 'record':True})
                             if intent_str == 'on':
                                 np_leds[:] = COLOR_MAP['white']
                             elif intent_str == 'off':
@@ -69,41 +84,10 @@ def main(train=False):
                                 words = p.split()
                                 for word in words:
                                     if word in COLOR_MAP:
-                                        np_leds[:] = COLOR_MAP[word]                    
+                                        np_leds[:] = COLOR_MAP[word]
         finally:
-            parent_conn.send(False)
-
-
-# def process_chime(conn):
-#     run = True
-#     device_index = None
-#     with Audio() as audio:
-#         device_index = None
-#         if bb_config['device'] == 'rpi':
-#             device_name = "USB Audio Device"
-#             device_index = -1
-#             for i in range(audio.get_device_count()):
-#                 device_info = audio.get_device_info_by_index(i)
-#                 if device_name in device_info["name"]:
-#                     device_index = i
-#                     break
-
-#             if device_index == -1:
-#                 print(f"Device '{device_name}' not found")
-#                 run = False
-#         chime_audio = None
-#         with wave.open(bb_config['chime_path'], 'rb') as chime:
-#             chime_config = {'format':audio.get_format_from_width(chime.getsampwidth()),
-#                             'channels':chime.getnchannels(),
-#                             'rate':chime.getframerate(),
-#                             'output':True,
-#                             'output_device_index': device_index}
-#             chime_audio = chime.readframes(chime.getnframes())
-#         with Speaker(audio, **chime_config) as speaker:
-#             while run:
-#                 run = conn.recv()
-#                 if run:
-#                     speaker.write(chime_audio)
+            parent_mic_conn.send({'run': False, 'record':True})
+            parent_speaker_conn.send(False)
 
 def process_speak(conn):
     dialogue.load_dialogue()
@@ -125,8 +109,9 @@ def process_speak(conn):
                 run = False
         previous_intent = "none"
         while run:
-            intent = conn.recv()
-            if isinstance(intent, str):
+            obj = conn.recv()
+            if isinstance(obj, dict):
+                intent = obj['intent']
                 if intent in dialogue.dialogue and previous_intent in dialogue.dialogue[intent]:
                     size = len(dialogue.dialogue[intent][previous_intent]) - 1
                     filename = f"{intent}_{previous_intent}_{random.randint(0, size)}.wav"
@@ -141,9 +126,52 @@ def process_speak(conn):
                         chime_audio = chime.readframes(chime.getnframes())
                     with Speaker(audio, **chime_config) as speaker:
                         speaker.write(chime_audio)
-                previous_intent = intent
+                if obj['reset']:
+                    previous_intent = "none"
+                else:
+                    previous_intent = intent
             else:
                 run = intent
+            conn.send(True)
         
-            
+def process_audio(conn: Connection):
+    run = True
+    device_index = None
+    with Audio() as audio:
+        if bb_config['device'] == 'rpi':
+            device_name = "USB Audio Device"
+            device_index = -1
+            for i in range(audio.get_device_count()):
+                device_info = audio.get_device_info_by_index(i)
+                if device_name in device_info["name"]:
+                    device_index = i
+                    break
+
+            if device_index == -1:
+                print(f"Device '{device_name}' not found")
+                run = False
+        config = {
+            'format': pyaudio.paInt16,
+            'channels': 1,
+            'rate': IN_RATE,
+            'input': True,
+            'frames_per_buffer': CHUNK_SIZE,
+            'input_device_index': device_index
+        }
+        with Microphone(audio, **config) as mic:
+            record = True
+            while run:
+                if record:
+                    audio = mic.read(CHUNK_SIZE, exception_on_overflow=False)
+                    conn.send_bytes(audio)
+                if conn.poll():
+                    obj = conn.recv()
+                    run = obj['run']
+                    if record and not obj['record']:
+                        mic.stop_stream()
+                        record = False
+                    elif not record and obj['record']:
+                        mic.start_stream()
+                        record = True
+                    
 
--- barkbright/models/asr.py
+++ barkbright/models/asr.py
@@ -19,94 +19,52 @@ import json
 import vosk
 import time
 from vosk import Model, KaldiRecognizer
-from pathlib import Path
 from scipy import signal
 import numpy as np
-from barkbright import Audio, Microphone, bb_config, CHUNK_SIZE, IN_RATE
-from multiprocessing import Process, Pipe
-from multiprocessing.connection import Connection
+from barkbright import bb_config, IN_RATE
 
 MODEL_RATE = 16000
 MAX_INT16 = (2**15 - 1)
 vosk.SetLogLevel(-1)
 
-def listen(model_path=None) -> str:
-    model = model_path if model_path else Model(model_path=bb_config['vosk_model_path'])
+def listen(parent_conn):
+    model = Model(model_path=bb_config['vosk_model_path'])
     recognizer = KaldiRecognizer(model, MODEL_RATE)
     recognizer.SetWords(True)
     recognizer.SetPartialWords(True)
-    parent_conn, child_conn = Pipe()
-    prcs = Process(target=process_audio, args=(child_conn,))
-    prcs.start()
     wake_word = False
     sleep_word = False
     start = None
     print("Running...")
-    try:
-        while True:
-            audio = b''
-            while parent_conn.poll():
-                audio += parent_conn.recv_bytes()
-            np_audio = np.frombuffer(audio, dtype=np.int16)
-            np_audio_float = np_audio.astype(np.float32, order='C') / MAX_INT16
-            np_audio_float = signal.resample_poly(np_audio_float, MODEL_RATE, IN_RATE)
-            np_audio = (np_audio_float * MAX_INT16).astype(np.int16)
-            audio = np_audio.tobytes()
-            if wake_word:
-                start = time.time_ns()
-            if recognizer.AcceptWaveform(audio):
-                phrase = str(json.loads(recognizer.Result())['text'])
-                wake_word = bb_config['wakeword'] in phrase or wake_word
-                sleep_word = bb_config['sleep_word'] in phrase or sleep_word
-                if phrase and wake_word:
-                    if sleep_word:
-                        index = phrase.find(bb_config['sleep_word'])
+    while True:
+        audio = b''
+        while parent_conn.poll():
+            audio += parent_conn.recv_bytes()
+        np_audio = np.frombuffer(audio, dtype=np.int16)
+        np_audio_float = np_audio.astype(np.float32, order='C') / MAX_INT16
+        np_audio_float = signal.resample_poly(np_audio_float, MODEL_RATE, IN_RATE)
+        np_audio = (np_audio_float * MAX_INT16).astype(np.int16)
+        audio = np_audio.tobytes()
+        if recognizer.AcceptWaveform(audio):
+            phrase = str(json.loads(recognizer.Result())['text'])
+            wake_word = bb_config['wakeword'] in phrase or wake_word
+            sleep_word = bb_config['sleep_word'] in phrase or sleep_word
+            print(f"{phrase}:{wake_word}:{sleep_word}")
+            if phrase and wake_word:
+                if sleep_word:
+                    index = phrase.find(bb_config['sleep_word'])
+                    if index != -1:
                         phrase = phrase[:index]
-                        print(phrase)
-                        yield phrase
-                        wake_word = False
-                    else:
-                        index = phrase.find(bb_config['wakeword'])
+                    phrase = 'sleep_word' if phrase == '' else phrase
+                    wake_word = False
+                    sleep_word = False
+                else:
+                    index = phrase.find(bb_config['wakeword'])
+                    if index != -1:
                         phrase = phrase[:index] + phrase[index+len(bb_config['wakeword']):]
-                        phrase = 'wakeword' if phrase == '' else phrase
-                        print(phrase)
-                        yield phrase
-            else:
-                partial_result = json.loads(recognizer.PartialResult())['partial'].lower()
-                wake_word = bb_config['wakeword'] in partial_result or wake_word
-                sleep_word = bb_config['sleep_word'] in partial_result or sleep_word
-
-
-    finally:
-        parent_conn.send(False)
-
-def process_audio(conn: Connection):
-    run = True
-    device_index = None
-    with Audio() as audio:
-        if bb_config['device'] == 'rpi':
-            device_name = "USB Audio Device"
-            device_index = -1
-            for i in range(audio.get_device_count()):
-                device_info = audio.get_device_info_by_index(i)
-                if device_name in device_info["name"]:
-                    device_index = i
-                    break
-
-            if device_index == -1:
-                print(f"Device '{device_name}' not found")
-                run = False
-        config = {
-            'format': pyaudio.paInt16,
-            'channels': 1,
-            'rate': IN_RATE,
-            'input': True,
-            'frames_per_buffer': CHUNK_SIZE,
-            'input_device_index': device_index
-        }
-        with Microphone(audio, **config) as mic:
-            while run:
-                audio = mic.read(CHUNK_SIZE, exception_on_overflow=False)
-                conn.send_bytes(audio)
-                if conn.poll():
-                    run = conn.recv()
+                    phrase = 'wakeword' if phrase == '' else phrase
+                yield phrase, sleep_word
+        else:
+            partial_result = json.loads(recognizer.PartialResult())['partial'].lower()
+            wake_word = bb_config['wakeword'] in partial_result or wake_word
+            sleep_word = bb_config['sleep_word'] in partial_result or sleep_word

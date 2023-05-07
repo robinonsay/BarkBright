1081f28f11afc0f55cc5f126e55aa6793183b491
Robin Onsay <hello@robinonsay.com>
2023-05-07 15:12:22 -0500

    :bug: fix neopixel iterator

--- barkbright/iot/neopixel.py
+++ barkbright/iot/neopixel.py
@@ -58,7 +58,7 @@ class NeoPixelLEDStrip:
     
     def __iter__(self):
         if IS_RPI:
-            for i in range(self._current_strip):
+            for i in range(len(self)):
                 yield self._current_strip[i]
 
     def show(self):

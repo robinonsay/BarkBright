543bfe88e3f42cf86d0a695a4eb3648f3544fff2
Robin Onsay <hello@robinonsay.com>
2023-05-13 13:50:32 -0500

    :bug: fix arglights

--- barkbright/iot/neopixel.py
+++ barkbright/iot/neopixel.py
@@ -136,7 +136,7 @@ def party_mode(neo_leds:NeoPixelLEDStrip, run_function:Value, fft_conn:Connectio
             bass = np.mean(audio_fft[16:256])
             MAX_BASS = bass if bass > MAX_BASS else MAX_BASS
             bass_norm = bass / MAX_BASS
-            arglights = int(bass_norm * neo_leds.strip.shape[0] // 2)
+            arglights = int((1 - bass_norm) * neo_leds.strip.shape[0] // 2)
             neo_leds.strip[:arglights] = (0,0,0)
             neo_leds.strip[arglights:-arglights] = party_colors[arglights:-arglights]
             neo_leds.strip[-arglights:] = (0,0,0)
